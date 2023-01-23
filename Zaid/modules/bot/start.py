1f8a8a19594c3a568dbd3229cfeb56024fc3668d
Raj pomane <95400073+Rajpomane@users.noreply.github.com>
2023-01-23 12:03:33 +0530

    Update start.py

--- Zaid/modules/bot/start.py
+++ Zaid/modules/bot/start.py
@@ -4,5 +4,4 @@ from pyrogram import filters
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
-   await message.reply_text("●[ Bʌcʜʜo ĸo pʜŋ📱 ʌʋr cʜʋtɩƴO 💦ĸo ɗɩɭ ɗoʛɘ♥ to wo ʛʌɱɘ ʜɩ ĸʜɘɭɘŋʛɘ ]🥴🤣
- 𝐎𝐖𝐍𝐄𝐑 : @END_OF_THE_LIFEE_ERROR")
+   await message.reply_text("●[ Bʌcʜʜo ĸo pʜŋ📱 ʌʋr cʜʋtɩƴO 💦ĸo ɗɩɭ ɗoʛɘ♥ to wo ʛʌɱɘ ʜɩ ĸʜɘɭɘŋʛɘ ]🥴🤣")
